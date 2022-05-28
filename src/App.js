8b483ab3458f7356c55b3e2ffc70518002ee9998
sumukha.m <sumukhamohan6@gmail.com>
2022-05-28 17:07:04 +0530

    feat(header): added header and material ui library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./public/styles/styles.css";
 import { routes } from "./routes";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/layouts/Header";
 
 function App() {
   return (
     <Router>
-      <div className="App">Hello world</div>
+      <Header />
       <Routes>
         {routes.map((routeObj) => (
           <Route key={routeObj.path} path={routeObj.path} element={routeObj.element} />
